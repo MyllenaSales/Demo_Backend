4766a91ea1ede10af0a7fe8b71d7b66ea9da7dad
MyllenaSales <myllenasales50@gmail.com>
2024-12-04 13:59:31 -0300

    add webConfig

--- src/main/java/br/edu/ifba/demo/backend/api/config/WebConfig.java
+++ src/main/java/br/edu/ifba/demo/backend/api/config/WebConfig.java
@@ -1,4 +1,4 @@
-package br.edu.ifba.demo.backend.config;
+package br.edu.ifba.demo.backend.api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
